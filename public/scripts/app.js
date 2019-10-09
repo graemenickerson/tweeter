e7ef2fd81809508d1f57440e7dcb97918fe025b4
graemenickerson <graeme.nickerson@gmail.com>
2019-10-09 02:41:36 +0000

    Adds broken code for dynamic tweets.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,7 +4,30 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-{/* <script type="text/javascript" src="/vendor/jquery-2.2.3.min.js"></script> */}
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
 
 const timeAgo = (givenDate) => {
   let result = 0;
@@ -36,60 +59,41 @@ const timeAgo = (givenDate) => {
   return result;
 };
 
+
+const createTweetElement = function(input) {
+
+  let $tweet = $('<article>').addClass('tweet');
+
+  const markup = `
+    <header>
+      <div>
+        <img src="${input.user.avatars}">
+        <h3>${input.user.name}</h3>
+      </div>
+      <h4>${input.user.handle}</h4>
+    </header>
+    <p>${input.content.text}</p>
+    <footer>
+      <h6>${timeAgo(input.created_at)}</h6>
+      <span name="interactions">
+        <h6>like and such</h6>
+      </span>
+    </footer>
+  `;
+
+  $($tweet).append(markup);
+
+  return $tweet;
+};
+
 const renderTweets = function(tweets) {
   // loops through tweets
   // calls createTweetElement for each tweet
   // takes return value and appends it to the tweets container
-  let result = [];
-  for (let tweet in tweets) {
-
+  for (let tweet of tweets) {
+    $('#tweet').append(createTweetElement(tweet));
   }
-  
-}
-
-const createTweetElement = (input) => {
-  const $tweet = `
-    <div class="tweet">
-      <article>
-        <header>
-          <div>
-            <img src="${input.user.avatars}">
-            <h3>${input.user.name}</h3>
-          </div>
-          <h4>${input.user.handle}</h4>
-        </header>
-        <p>${input.content.text}</p>
-        <footer>
-          <h6>${timeAgo(input.created_at)}</h6>
-          <span name="interactions">
-            <h6>like and such</h6>
-          </span>
-        </footer>
-      </article>
-    </div>
-  `;
-    
-  
-  return $tweet;
 };
 
-
-// Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
-    },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
-}
-
-const $tweet = createTweetElement(tweetData);
-
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got
-// all the right elements, classes, etc.
+// createTweetElement(data[0]);
+renderTweets(data);
