77cd5028a80dd4f3ce42c4cc46ec29e25f8fe22f
graemenickerson <graeme.nickerson@gmail.com>
2019-10-09 03:18:13 +0000

    Refactors timeAge function in app.js to make more readable.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -28,25 +28,31 @@ const timeAgo = (givenDate) => {
   let result = 0;
   let dateNow = new Date();
   const timeDiff  = (dateNow - givenDate);
+  
+  const min = 1000 * 60;
+  const hour = 1000 * 60 * 60;
+  const day = 1000 * 60 * 60 * 24;
+  const month = 1000 * 60 * 60 * 24 * 30;
+  const year = 1000 * 60 * 60 * 24 * 365;
 
-  if ((timeDiff / (1000 * 60 * 60 * 24 * 365) >= 2)) {// years
-    result = Math.round(timeDiff / (1000 * 60 * 60 * 24 * 365)) + ' years';
-  } else if ((timeDiff / (1000 * 60 * 60 * 24 * 365) >= 1)) {// year
-    result = Math.round(timeDiff / (1000 * 60 * 60 * 24 * 365)) + ' year';
-  } else if ((timeDiff / (1000 * 60 * 60 * 24 * 30) >= 2)) {// months
-    result = Math.round(timeDiff / (1000 * 60 * 60 * 24 * 30)) + ' months';
-  } else if ((timeDiff / (1000 * 60 * 60 * 24 * 30) >= 1)) {// month
-    result = Math.round(timeDiff / (1000 * 60 * 60 * 24 * 30)) + ' month';
-  } else if ((timeDiff / (1000 * 60 * 60 * 24) >= 2)) {// days
-    result = Math.round(timeDiff / (1000 * 60 * 60 * 24)) + ' days';
-  } else if (timeDiff / (1000 * 60 * 60 * 24) >= 1) {// day
-    result = Math.round(timeDiff / (1000 * 60 * 60 * 24)) + ' day';
-  } else if (timeDiff / (1000 * 60 * 60) >= 2) {// hours
-    result = Math.round(timeDiff / (1000 * 60 * 60)) + ' hours';
-  } else if ((timeDiff / (1000 * 60 * 60) >= 1)) {// hour
-    result = Math.round(timeDiff / (1000 * 60 * 60)) + ' hour';
-  } else if (timeDiff / (1000 * 60) >= 1) {//minutes
-    result = Math.round(timeDiff / (1000 * 60)) + ' minutes';
+  if ((timeDiff / (year) >= 2)) {// years
+    result = Math.round(timeDiff / (year)) + ' years';
+  } else if ((timeDiff / (year) >= 1)) {// year
+    result = Math.round(timeDiff / (year)) + ' year';
+  } else if ((timeDiff / (month) >= 2)) {// months
+    result = Math.round(timeDiff / (month)) + ' months';
+  } else if ((timeDiff / (month) >= 1)) {// month
+    result = Math.round(timeDiff / (month)) + ' month';
+  } else if ((timeDiff / (day) >= 2)) {// days
+    result = Math.round(timeDiff / (day)) + ' days';
+  } else if (timeDiff / (day) >= 1) {// day
+    result = Math.round(timeDiff / (day)) + ' day';
+  } else if (timeDiff / (hour) >= 2) {// hours
+    result = Math.round(timeDiff / (hour)) + ' hours';
+  } else if ((timeDiff / (hour) >= 1)) {// hour
+    result = Math.round(timeDiff / (hour)) + ' hour';
+  } else if (timeDiff / (min) >= 1) {//minutes
+    result = Math.round(timeDiff / (min)) + ' minutes';
   } else {
     result = timeDiff / (1000) + ' seconds';
   }
