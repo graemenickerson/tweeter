8479edd255c4c50ebf5c82d8768270151ca4ecc3
graemenickerson <graeme.nickerson@gmail.com>
2019-10-10 01:30:15 +0000

    Adds a back to top button to Tweeter.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,10 @@
+// composer-char-counter.js
+//Graeme Nickeson
+// October 2019
+
 
 $(document).ready(function() {
-  // --- our code goes here ---
+  // Updates the label below the form input to show remaining characters available.
   $('[name="text"]').keyup(function() {
     const numOfChars = $(this).val().length;
     const $counter = $(this).siblings()[1];
@@ -11,4 +15,22 @@ $(document).ready(function() {
       $($counter).text(counterValue).removeClass('neg');
     }
   });
+
+  // Tracks the scroll level of the window and shows the to top button
+  // when scrolled down.
+  $(document).scroll(function() {
+    if($(document).scrollTop() > 20) {
+      document.getElementById("to-top").style.display = "block";
+    } else {
+      document.getElementById("to-top").style.display = "none";
+    }
+  });
+
+  // Scrolls the window to the top and toggles the new tweet form
+  $("#to-top").click(function() {
+     $("HTML, BODY").animate({ scrollTop: 0 }, 1000); 
+     $('#new-post').slideToggle("slow");
+    $('textarea').focus();
+  }); 
+
 });
